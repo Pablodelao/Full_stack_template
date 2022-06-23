a7abdf2bf16a9672147d8e1e790fb0351e05d76d
Elmega123 <yourpalbussiness@gmail.com>
2022-06-23 00:23:55 -0500

    added variables and mongo connection

--- server.js
+++ server.js
@@ -7,3 +7,21 @@ const app = express()
 const cors = require("cors")
 const MongoClient = require("mongodb").MongoClient
 require('dotenv').config() 
+
+// Now we declare some variables this is so that we can connect to the database
+// The following is to make sure we are able to get our connection string. Since it will be in another file that won't be pushed
+// We hide secret things on .env
+// we put our connection string on my .env
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = "sample_mflix",
+    collection 
+// this is so that we connect 
+MongoClient.connect(dbConnectionString)
+// after that we consolelog that we connected to database and we set the db to the name of the database and collection we are gonna use
+    .then(client => {
+        console.log('Connected to database')
+        db = client.db(dbName)
+        collection = db.collection('movies')
+
+    })
